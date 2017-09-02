9e6e94bb5ca50a04edb0fa3d311898ae9b1ccb9a
Ouye Xie <ouyexie@amazon.com>
2017-09-02 09:12:43 -0700

    fix naming

--- relay-fullstack/client/components/Analyzer/AnalyzerContainer.js
+++ relay-fullstack/client/components/Analyzer/AnalyzerContainer.js
@@ -1,8 +1,8 @@
 import Relay from 'react-relay';
-import App from './AnalyzerComponent';
+import Analyzer from './AnalyzerComponent';
 import Footer from '../Footer/FooterContainer';
 
-export default Relay.createContainer(App, {
+export default Relay.createContainer(Analyzer, {
   fragments: {
     viewer: () => Relay.QL`
       fragment on User {
